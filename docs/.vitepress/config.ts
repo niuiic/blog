f289a78d4ac9a55e7468ade366a3fa91846c8abc
niuiic <niuiicde@gmail.com>
2022-09-08 15:09:26 +0800

    update layout

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,50 @@ export default {
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.png' }]],
   themeConfig: {
     siteTitle: "niuiic's Blog",
-    logo: '/favicon.png'
+    logo: '/favicon.png',
+    nav: [
+      // {
+      //   text: 'Drop Menu',
+      //   items: [
+      //     {
+      //       items: [
+      //         { text: 'Item A1', link: '/item-A1' },
+      //         { text: 'Item A2', link: '/item-A2' }
+      //       ]
+      //     },
+      //     {
+      //       items: [
+      //         { text: 'Item B1', link: '/item-B1' },
+      //         { text: 'Item B2', link: '/item-B2' }
+      //       ]
+      //     }
+      //   ]
+      // },
+      { text: 'Articles', link: '/articles/index' }
+    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/niuiic' }]
+    // sidebar: [
+    //   {
+    //     text: '组件库源码实现',
+    //     collapsible: true,
+    //     collapsed: true,
+    //     items: [
+    //       {
+    //         text: '组件库环境搭建',
+    //         link: '/articles/组件库环境搭建'
+    //       },
+    //       { text: 'gulp的使用', link: '/articles/gulp的使用' }
+    //     ]
+    //   },
+    //   {
+    //     text: 'vue教程',
+    //     items: [
+    //       {
+    //         text: 'pina和vuex',
+    //         link: '/articles/pina和vuex'
+    //       }
+    //     ]
+    //   }
+    // ]
   }
 }
