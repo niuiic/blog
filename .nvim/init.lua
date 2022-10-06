2ec24a08b2ee4839004cf8d375d16ce64d93f109
niuiic <niuiicde@gmail.com>
2022-10-06 22:47:58 +0800

    update tasks

--- .nvim/init.lua
+++
@@ -1,5 +0,0 @@
-local utils = require("utils")
-
-package.path = debug.getinfo(1, "S").source:match([[^@?(.*[\/])[^\/]-$]]) .. "?.lua;" .. package.path
-
-utils.fn.require("task")
--- .nvim/task/task.sh
+++
@@ -1,6 +0,0 @@
-if [ "$1" = "preview" ]; then
-	pnpm docs:dev
-elif [ "$1" = "test" ]; then
-	pnpm docs:build
-	pnpm docs:serve
-fi
